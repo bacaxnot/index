bf7cd1bebcf8af2859ddefba43363bcd848ebd3e
bacaxnot <bacaxnot@icloud.com>
2024-03-17 06:51:51 -0500

    [admin] update help command to add packages

--- apps/admin/src/lib/cli/instances/commands/help.ts
+++ apps/admin/src/lib/cli/instances/commands/help.ts
@@ -4,8 +4,33 @@ export const help = Builder.command()
   .name("help")
   .description("shows this help message.")
   .action(async ({ terminal }) => {
-    return Object.values(terminal.commands).reduce((acc, command) => {
-      return `${acc}${command.name.padEnd(10)} - ${command.description}  \n`;
-    }, "");
+    const nextLine = "  \n";
+    const blankLine = "\n\n";
+
+    const commandsArray = Object.values(terminal.commands);
+    const commands =
+      commandsArray.length == 0
+        ? "no registered commands."
+        : commandsArray.reduce((acc, command) => {
+            return `${acc}${command.name.padEnd(10)} - ${command.description}${nextLine}`;
+          }, "");
+
+    const packagesArray = Object.values(terminal.packages);
+    const packages =
+      packagesArray.length == 0
+        ? "no registered packages."
+        : packagesArray.reduce((acc, pkg) => {
+            return `${acc}${pkg.name.padEnd(10)} - ${pkg.description}${nextLine}`;
+          }, "");
+
+    return (
+      "commands:" +
+      blankLine +
+      commands +
+      blankLine +
+      "packages:" +
+      blankLine +
+      packages
+    );
   })
   .build();
