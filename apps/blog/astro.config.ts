c48e96064ddbc976ccadc6fd583c3f5b7a301e96
bacaxnot <bacaxnot@icloud.com>
2024-03-20 11:28:31 -0500

    [blog] move to sst

--- apps/blog/astro.config.ts
+++ apps/blog/astro.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import aws from 'astro-sst'
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,7 +17,5 @@ export default defineConfig({
 		]
 	},
 	integrations: [tailwind()],
-	adapter: vercel({
-		imageService: true
-	})
+	adapter: aws()
 })
