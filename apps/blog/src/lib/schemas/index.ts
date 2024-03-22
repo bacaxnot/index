7873be1b0be98f0e01518a869aeef9e785ec077e
bacaxnot <bacaxnot@icloud.com>
2024-03-22 07:24:56 -0500

    [blog] remove supabase client

--- apps/blog/src/lib/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-import type { Database } from '../supabase/types'
-
-type User = Database['public']['Tables']['users']['Row']
-type Post = Database['public']['Tables']['posts']['Row']
-
-export type BlogPost = Post & { user: User | null }
